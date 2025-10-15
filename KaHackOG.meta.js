f6a9917725202a2320515bac62dfa3a88854d4ee
slaytermarshall12 <slaytermarshall12@gmail.com>
2025-10-15 13:01:31 +1000

    Update KaHackOG.meta.js

--- KaHackOG.meta.js
+++ KaHackOG.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         KaHack!
+// @name         cameron D
 // @version      1.0.24
 // @version      1.0.25
 // @namespace    https://github.com/jokeri2222
@@ -7,7 +7,7 @@
 // @updateURL    https://github.com/jokeri2222/Kahoot-Hack/raw/main/KaHack!.meta.js
 // @downloadURL  https://github.com/jokeri2222/Kahoot-Hack/raw/main/KaHack!.user.js
 // @author       jokeri2222; https://github.com/jokeri2222
-// @match        https://kahoot.it/*
+// @match        https://kahoot.it/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
 // @grant        none
 // ==/UserScript==
